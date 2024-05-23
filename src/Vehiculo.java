7ba01fdc2e740d34227249cb00e0dec9e434ff2e
POO <andresftma@gmail.com>
2024-05-23 07:57:10 -0500

    Hola mundo

---
+++ src/Vehiculo.java
@@ -0,0 +1,36 @@
+public class Vehiculo {
+    String placa;
+    String marca;
+    public Vehiculo(String placa, String marca) {
+        this.placa=placa;
+        this.marca=marca;
+    }
+    public void imprimir(){
+        System.out.println(placa+" "+marca);
+    }
+}
+class Publico extends Vehiculo{
+    String servicio;
+
+    public Publico(String placa,String marca,String servicio){
+        super(placa,marca);
+        this.servicio=servicio;
+    }
+    @Override
+    public void imprimir() {
+        super.imprimir();
+    }
+}
+class Privado extends Vehiculo{
+    String modelo;
+    String propietario;
+    public Privado(String placa,String marca,String modelo,String propietario){
+        super(placa,marca);
+        this.modelo=modelo;
+        this.propietario=propietario;
+    }
+    @Override
+    public void imprimir() {
+        super.imprimir();
+    }
+}
