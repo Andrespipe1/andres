2f406a02021f55c55f7d7ff3144952e702ec8411
POO <andresftma@gmail.com>
2024-05-23 09:51:02 -0500

    ejercicio en clase 2.0

--- src/ejercicio3/Main.java
+++ src/ejercicio3/Main.java
@@ -2,7 +2,7 @@ package ejercicio3;
 
 public class Main {
     public static void main (String[] args){
-        Duenio mascota1=new Duenio("Gato","Siames",2,"Andres","Tufiño",19,"El inca",1);
-        mascota1.imprimir();
+        Veterinario mascota1=new Veterinario("Gato","Siames",2,"Andres","Tufiño",19,"El inca",1);
+        mascota1.examinarMascota();
     }
 }
--- src/ejercicio3/Mascota.java
+++ src/ejercicio3/Mascota.java
@@ -4,29 +4,42 @@ public class Mascota {
     String animal;
     String raza;
     int edad;
+
     public Mascota(String animal, String raza, int edad) {
         this.animal = animal;
         this.raza = raza;
-        this.edad=edad;
+        this.edad = edad;
     }
 }
-class Duenio extends Mascota {
+
+    class Duenio {
     String nombre;
     String apellido;
-    int edad_duenio;
+    int edad;
     String direccion;
     int n_mascotas;
 
-    public Duenio(String animal,String raza,int edad, String nombre, String apellido, int edad_duenio, String direccion,int n_mascotas){
-        super(animal,raza,edad);
-        this.nombre=nombre;
-        this.apellido=apellido;
-        this.edad_duenio=edad_duenio;
-        this.direccion=direccion;
-        this.n_mascotas=n_mascotas;
-
+    public Duenio(String nombre, String apellido, int edad, String direccion, int n_mascotas) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.direccion = direccion;
+        this.n_mascotas = n_mascotas;
     }
-    public void imprimir(){
-        System.out.println("---DATOS MASCOTA---\nAnimal: "+animal+"\nRaza: "+raza+"\nEdad: "+edad+"\n---DATOS DUEÑO---\n"+"Nombre: "+nombre+" "+apellido+"\nEdad: "+edad_duenio+"\nDirección: "+direccion+"\nN° de mascotas: "+n_mascotas);
+}
+
+    class Veterinario {
+    String nombre;
+    String apellido;
+    String nombre_clinica;
+
+    public Veterinario(String nombre, String apellido, String nombre_clinica) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.nombre_clinica = nombre_clinica;
+    }
+
+    public void examinarMascota(Mascota mascota) {
+
     }
 }
